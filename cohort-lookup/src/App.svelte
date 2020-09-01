12a36a4a129933493157d68d0de1f4d02a065771
ruoshui-git <ruoshuim@gmail.com>
2020-08-31 20:10:51 -0400

    feat: better styles

--- cohort-lookup/src/App.svelte
+++ cohort-lookup/src/App.svelte
@@ -18,13 +18,34 @@
   main {
     text-align: center;
     padding: 1em;
-    max-width: 240px;
+    /* max-width: 240px; */
     margin: 0 auto;
   }
 
   span {
-	  color: green;
-		
+    color: green;
+  }
+
+  .result {
+	font-size: 2rem;
+    padding: 1.5rem 2rem;
+    border-radius: 0.2rem;
+    margin: 0 auto;
+  }
+
+  input {
+    font-family: "Roboto", sans-serif;
+    color: #333;
+    font-size: 1rem;
+    margin: 0 auto;
+    padding: 1.5rem 2rem;
+    border-radius: 0.2rem;
+    background-color: rgb(255, 255, 255);
+    /* border: none; */
+    width: 90%;
+    display: block;
+    /* border-bottom: 0.3rem solid black; */
+    transition: all 0.3s;
   }
 
   h1 {
@@ -32,12 +53,6 @@
     text-transform: uppercase;
     font-size: 4em;
     font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
   }
 </style>
 
@@ -47,10 +62,14 @@
 
 <main>
   <h1>Hello Stuy!</h1>
+  <p>Get your cohort</p>
   <input type="text" placeholder="Your OSIS here" bind:value={query} />
-  {#if lookupResult}
-    <p>Your group is: <span>{lookupResult}</span></p>
-  {:else}
-    <p class="warning">OSIS not found</p>
-  {/if}
+  <div class="result">
+    {#if lookupResult}
+      <code>Your group is: <span>{lookupResult}</span></code>
+    {:else}
+      <p>OSIS not found</p>
+    {/if}
+  </div>
+  <p>Original spreedsheet & more descriptions <a target="_blank" href="https://tinyurl.com/Stuy-Blended">here</a></p>
 </main>
